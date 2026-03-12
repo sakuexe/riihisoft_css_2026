81f443e73246ddfd6f8e0b088c8c488d463c2c93
Saku Karttunen <saku.karttunen@gmail.com>
2026-03-12 19:35:56 +0200

    first version of the example page created

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+//@ts-ignore
+lucide.createIcons();
+
+/** @type {HTMLTextAreaElement | null} */
+const cssEditor = document.querySelector("#css-editor");
+/** @type {HTMLStyleElement | null} */
+const styleBlock = document.querySelector("aside style");
+
+
+function updateStyle() {
+  if (!cssEditor) {
+    throw new Error("could not find `#css-editor`");
+  }
+  if (!styleBlock) {
+    throw new Error("could not find `aside style`");
+  }
+
+  styleBlock.textContent = cssEditor.value;
+}
+
+cssEditor?.addEventListener("input", updateStyle);
+updateStyle();
+
+/** @type {HTMLButtonElement | null} */
+const cssEditorToggle = document.querySelector(".example-page button.toggle");
+/** @type {HTMLDivElement | null} */
+const editorWindow = document.querySelector(".example-page + aside:has(#css-editor)");
+
+cssEditorToggle?.addEventListener("click", (_) => {
+  if (!editorWindow) {
+    throw new Error("could not find `.example-page + aside:has(#css-editor)`");
+  }
+
+  editorWindow.ariaExpanded = editorWindow.ariaExpanded === "true"
+    ? "false"
+    : "true"
+})
