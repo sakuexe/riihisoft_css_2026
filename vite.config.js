5da5f958a379bdc022f0a00ecfb344189817c1e0
Saku Karttunen <saku.karttunen@gmail.com>
2026-03-26 17:40:20 +0200

    added github pages workflow

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite"
 
-export default defineConfig({
-  root: "src"
+export default defineConfig(({ command }) => {
+  return {
+    root: "src",
+    base: command === "serve" ? "/" : "/riihisoft_css_2026/",
+  }
 })
