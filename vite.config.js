752fe5ad74a8a7a6f567509b962a51dbf3085fe7
Saku Karttunen <saku.karttunen@gmail.com>
2026-03-26 17:46:18 +0200

    fix to build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ export default defineConfig(({ command }) => {
   return {
     root: "src",
     base: command === "serve" ? "/" : "/riihisoft_css_2026/",
+    build: {
+      outDir: "../dist",
+      emptyOutDir: true,
+    }
   }
 })
